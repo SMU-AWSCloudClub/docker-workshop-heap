56a5fbe8ea30fd7e71744a7d1dce34e652e9f6b0
Brian Chew <brian.chew.2022@scis.smu.edu.sg>
2024-07-05 00:51:34 +0800

    asd

--- start-wssl.sh
+++ start-wssl.sh
@@ -5,15 +5,27 @@ export PUBLIC_IP='books.bchwy.com' # TODO: replace with your domain
 export NEXT_PUBLIC_BACKEND_URL="https://${PUBLIC_IP}/api"
 
 # Run docker-compose with the environment variable
-docker compose -f docker-compose.cloud-ssl.yaml up --build -d
+docker-compose -f docker-compose.cloud-ssl.yaml up --build -d
 
 # Wait for the services to start
 sleep 10
 
+# Check the status of the containers
+docker-compose -f docker-compose.cloud-ssl.yaml ps
+
 # Obtain SSL certificate using Certbot
-docker compose run --rm certbot certonly --webroot --webroot-path=/var/www/certbot -d $PUBLIC_IP
+docker-compose -f docker-compose.cloud-ssl.yaml run --rm certbot certonly --webroot --webroot-path=/var/www/certbot -d $PUBLIC_IP
 
 # Reload Nginx to apply the new certificate
-docker compose -f docker-compose.cloud-ssl.yaml exec nginx nginx -s reload
+docker-compose -f docker-compose.cloud-ssl.yaml exec nginx nginx -s reload
+
+# Check the status of the containers again
+docker-compose -f docker-compose.cloud-ssl.yaml ps
+
+# Check logs for any issues
+# docker-compose -f docker-compose.cloud-ssl.yaml logs nginx
+# docker-compose -f docker-compose.cloud-ssl.yaml logs backend
+# docker-compose -f docker-compose.cloud-ssl.yaml logs frontend
+# docker-compose -f docker-compose.cloud-ssl.yaml logs certbot
 
 echo "SSL certificate obtained and Nginx reloaded."
