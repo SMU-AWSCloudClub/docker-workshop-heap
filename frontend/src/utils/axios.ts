55bfe10246faa2c9faca39934ffc99082988a47c
Brian Chew <brian.chew.2022@scis.smu.edu.sg>
2024-07-04 10:29:27 +0800

    Nginx

--- frontend/src/utils/axios.ts
+++ frontend/src/utils/axios.ts
@@ -1,11 +1,7 @@
 import axios from 'axios';
 
-const isBrowser = typeof window !== 'undefined';
-
 const instance = axios.create({
-  baseURL: isBrowser
-    ? 'http://localhost:8080'  // Use localhost when in browser
-    : 'http://backend:8080',   // Use service name when in Docker
+  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
 });
 
 export default instance;
