be178ca2704c5921a3c82c0d5aec9a3404c57557
Brian Chew <brian.chew.2022@scis.smu.edu.sg>
2024-07-05 00:09:52 +0800

    asdx

--- start-nossl.sh
+++ start-nossl.sh
@@ -2,25 +2,8 @@
 
 # Set the public IP or domain
 # Uncomment the following line if using non-SSL
-# export PUBLIC_IP=$(curl -s http://checkip.amazonaws.com)
-
-# This is for SSL
-export PUBLIC_IP='books.bchwy.com' # TODO: replace with your domain
-export NEXT_PUBLIC_BACKEND_URL="https://${PUBLIC_IP}/api"
-
-# Generate self-signed SSL certificate
-DOMAIN="books.bchwy.com" # Change this to your group domain
-SSL_DIR="/etc/nginx/ssl"
-NGINX_CONF="/etc/nginx/nginx.conf"
-
-# Create SSL directory if it doesn't exist
-sudo mkdir -p $SSL_DIR
-
-# Generate a self-signed SSL certificate
-sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
-    -keyout $SSL_DIR/$DOMAIN.key \
-    -out $SSL_DIR/$DOMAIN.crt \
-    -subj "/C=US/ST=State/L=City/O=Organization/OU=Department/CN=$DOMAIN"
+export PUBLIC_IP=$(curl -s http://checkip.amazonaws.com)
+export NEXT_PUBLIC_BACKEND_URL="http://${PUBLIC_IP}/api"
 
 # Update Nginx configuration
 sudo tee $NGINX_CONF >/dev/null <<EOL
@@ -33,20 +16,6 @@ http {
         listen 80;
         server_name $DOMAIN;
 
-        # Redirect all HTTP requests to HTTPS
-        return 301 https://\$host\$request_uri;
-    }
-
-    server {
-        listen 443 ssl;
-        server_name $DOMAIN;
-
-        ssl_certificate $SSL_DIR/$DOMAIN.crt;
-        ssl_certificate_key $SSL_DIR/$DOMAIN.key;
-
-        ssl_protocols TLSv1.2 TLSv1.3;
-        ssl_ciphers HIGH:!aNULL:!MD5;
-
         location / {
             proxy_pass http://frontend:3000;
             proxy_set_header Host \$host;
@@ -55,7 +24,7 @@ http {
             proxy_set_header X-Forwarded-Proto \$scheme;
         }
 
-        location /api/ {
+        location /api {
             proxy_pass http://backend:8080;
             proxy_set_header Host \$host;
             proxy_set_header X-Real-IP \$remote_addr;
@@ -66,7 +35,7 @@ http {
 }
 EOL
 
-echo "SSL certificate generated and Nginx configuration updated."
+echo "Nginx configuration updated."
 
 # Run docker-compose with the environment variable
-docker compose -f docker-compose.cloud-ssl.yaml up --build
+docker compose -f docker-compose.cloud.yaml up --build
