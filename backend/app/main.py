67013f64ff3adfc377fe1c2cbe5d71b542478530
Brian Chew <brian.chew.2022@scis.smu.edu.sg>
2024-07-04 21:39:12 +0800

    frnak

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends, HTTPException
+from fastapi import FastAPI, Depends, HTTPException, APIRouter
 from sqlalchemy.orm import Session
 from fastapi.middleware.cors import CORSMiddleware
 from typing import List
@@ -16,6 +16,7 @@ app.add_middleware(
     allow_headers=["*"],  # Allows all headers
 )
 
+
 # Dependency
 def get_db():
     db = SessionLocal()
@@ -24,26 +25,42 @@ def get_db():
     finally:
         db.close()
 
-@app.post("/books/", response_model=schemas.Book)
+
+router = APIRouter()
+
+
+@router.get("/")
+def hi():
+    return "hello world"
+
+
+@router.post("/books/", response_model=schemas.Book)
 def create_book(book: schemas.BookCreate, db: Session = Depends(get_db)):
     return crud.create_book(db=db, book=book)
 
-@app.get("/books/", response_model=List[schemas.Book])
+
+@router.get("/books/", response_model=List[schemas.Book])
 def read_books(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
     books = crud.get_books(db, skip=skip, limit=limit)
     return books
 
-@app.get("/books/{book_id}", response_model=schemas.Book)
+
+@router.get("/books/{book_id}", response_model=schemas.Book)
 def read_book(book_id: int, db: Session = Depends(get_db)):
     db_book = crud.get_book(db, book_id=book_id)
     if db_book is None:
         raise HTTPException(status_code=404, detail="Book not found")
     return db_book
 
-@app.put("/books/{book_id}", response_model=schemas.Book)
+
+@router.put("/books/{book_id}", response_model=schemas.Book)
 def update_book(book_id: int, book: schemas.BookCreate, db: Session = Depends(get_db)):
     return crud.update_book(db=db, book_id=book_id, book=book)
 
-@app.delete("/books/{book_id}", response_model=schemas.Book)
+
+@router.delete("/books/{book_id}", response_model=schemas.Book)
 def delete_book(book_id: int, db: Session = Depends(get_db)):
     return crud.delete_book(db=db, book_id=book_id)
+
+
+app.include_router(router, prefix="/api")
--- start.sh
+++ start.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-# Fetch the public IP
-export PUBLIC_IP=$(curl -s http://checkip.amazonaws.com)
+# Fetch the public IP ##! uncomment when going to deploy in Ec2 
+# export PUBLIC_IP=$(curl -s http://checkip.amazonaws.com)
+export PUBLIC_IP='localhost'
 
 # Run docker-compose with the environment variable
-docker-compose up -d
+docker-compose up -d --build
