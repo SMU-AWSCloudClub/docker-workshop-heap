6abc429f7ee201c0fa3598b2462f9e7c7476b3bd
Brian Chew <brian.chew.2022@scis.smu.edu.sg>
2024-07-05 00:36:43 +0800

    asd

--- start-locally.sh
+++ start-locally.sh
@@ -2,8 +2,7 @@
 
 # Set the public IP or domain
 # Uncomment the following line if using non-SSL
-export PUBLIC_IP=$(curl -s http://checkip.amazonaws.com)
-export NEXT_PUBLIC_BACKEND_URL="http://${PUBLIC_IP}/api"
+export NEXT_PUBLIC_BACKEND_URL="http://localhost/api"
 
 # Run docker-compose with the environment variable
 docker compose -f docker-compose.yaml up --build
