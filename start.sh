1724ca569b0bd21574c2fdd954eef50e2e6892a9
Brian Chew <brian.chew.2022@scis.smu.edu.sg>
2024-07-04 22:27:33 +0800

    fix

--- start.sh
+++ start.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-# Fetch the public IP ##! uncomment when going to deploy in Ec2 
-# export PUBLIC_IP=$(curl -s http://checkip.amazonaws.com)
-export PUBLIC_IP='localhost'
+# Fetch the public IP ##! uncomment when going to deploy in Ec2
+export PUBLIC_IP=$(curl -s http://checkip.amazonaws.com)
+# export PUBLIC_IP='localhost'
 
 # Run docker-compose with the environment variable
 docker-compose up -d --build
+
+#! Powershell use:
+#!$env:PUBLIC_IP = (Invoke-RestMethod http://checkip.amazonaws.com).Trim()
