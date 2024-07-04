2dfc7f37e15d83fbcf1b5cb88549ee449d1af8e8
Brian Chew <brian.chew.2022@scis.smu.edu.sg>
2024-07-04 23:40:23 +0800

    asd

--- frontend/src/utils/axios.ts
+++ frontend/src/utils/axios.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
-const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:8080';
-console.log('Backend URL:', backendUrl);  
+const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL;
+console.log('Backend URL:', backendUrl);
+
 
 const instance = axios.create({
   baseURL: backendUrl,
