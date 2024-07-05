21fce9b75c50c31651a155761cfa9efd86128751
Brian Chew <brian.chew.2022@scis.smu.edu.sg>
2024-07-05 19:11:14 +0800

    windows

--- start-locally.sh
+++ start-locally.sh
@@ -6,3 +6,5 @@ export NEXT_PUBLIC_BACKEND_URL="http://localhost/api"
 
 # Run docker-compose with the environment variable
 docker compose -f docker-compose.yaml up --build
+
+# Windows ENV: $env:NEXT_PUBLIC_BACKEND_URL = "http://localhost/api"
